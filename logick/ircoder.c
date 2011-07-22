f0fa0b49d732e1a9db246e647c95585960aef1b7
Leonid Myravjev <asm@asm.pp.ru>
2011-07-23 00:06:48 +0400

    Added IR decoder (but something is wrong, see FIXME notes).

--- logick/ircoder.c
+++ logick/ircoder.c
@@ -13,8 +13,8 @@
  *  Example:
  *   code == 0x05 -- 0b0101
  *  ircode:
- *   |header | 0 | 1  | 0 | 1  |
- *   |  1110 | 10| 100| 10| 100| == 11101010 01010000 == 0xEA 0x50
+ *   |header | 0 | 1  | 0 | 1  | stop|
+ *   |  1110 | 10| 100| 10| 100| 1   | == 11101010 01010010 == 0xEA 0x52
  */
 
 int code2ir_shot(uint8_t *code, uint8_t *result, int size_of_bites)
@@ -39,7 +39,77 @@ int code2ir_shot(uint8_t *code, uint8_t *result, int size_of_bites)
       if(++x>=8)
         result[++y]=x=0;
   }
+  result[y]|=1<<(7-x);
 
+  return 0;
+}
+
+/*
+ * Convert IR sequence to real code
+ *
+ * Package:
+ *  1 1 1 0 -- HEADER
+ *  1 0     -- Logick "0"
+ *  1 0 0   -- Logick "1"
+ *
+ *  Example:
+ *   code == 0xEA 0x50 == 11101010 01010000
+ *  ircode
+ *   |  1110 | 10| 100| 10| 100|
+ *   |header | 0 | 1  | 0 | 1  | == 0x05
+ */
+
+int ir_shot2code(uint8_t *ircode, uint8_t *code)
+{
+  int x,y;
+  *((uint64_t*)code)=y=0;
+  x=8;
+  // FIXME maybe header isn't exist.
+  int one=0;
+  int null=0;
+  int head=0;
+  int a,b;
+  a=b=0;
+  int max=100;
+  while(max--){
+    uint8_t times;
+    times=2;
+
+    int byte=(ircode[a]>>(7-b))&1;
+    if(byte == 1){
+      if(one==1){
+        if(null==0){
+          head=1;//header, drop one byte
+        }else if((null>1)){
+          if(head==1){
+            head=0;
+            continue;
+          }
+            code[y]|=1<<x;
+            if(x--==0){
+              x=7;
+              code[++y]=0;
+            }
+        }else{
+          if(x--==0){
+            x=7;
+            code[++y]=0;
+          }
+        }
+      }
+      null=0;
+      one=1;
+    }else{
+      null++;
+    }
+    if((one==1) && (null>2)){
+      break;
+    }
+    if(++b>=8){
+      b=0;
+      a++;
+    }
+  }
   return 0;
 }
 
@@ -69,19 +139,30 @@ int main(void)
 
   st=0x0000000000000000ll;
   code2ir_shot(code, rcode, 4);
-  assert(*result== 0xa0ea);
+  //printf("%llx\n", *result);
+  assert(*result== 0xa8ea);
+  ir_shot2code(rcode, code);
+  assert(*((uint64_t*)code) == 0x00);
+  //printf("%llx\n", *((uint64_t*)code));
+
 
   st=0x0000000000000005ll;
   code2ir_shot(code, rcode, 4);
-  assert(*result== 0x50ea);
+  assert(*result== 0x52ea);
+  ir_shot2code(rcode, code);
+  assert(*((uint64_t*)code) == 0x05<<4);// Four byte
 
   st=0x0000000000000055ll;
   code2ir_shot(code, rcode, 8);
-  assert(*result == 0x9452ea);
+  assert(*result == 0x809452ea);
+  ir_shot2code(rcode, code);
+  assert(*((uint64_t*)code) == 0x55);
 
-  st=0x0987654321ll;
+  st=0x0987654321ll;//FIXME
   code2ir_shot(code, rcode, 40);
   assert(*result == 0x522a29495552a9ea);
+  ir_shot2code(rcode, code);
+  assert(*((uint64_t*)code) == 0x2043658709);// FIXME bad decoder 0x20 but send 0x21
 
   printf("All test success!\n");
 
