e1f523b0e06a05281e1b018b115902c2d1e714dc
Leonid Myravjev <asm@asm.pp.ru>
2011-07-30 00:44:01 +0400

    Rewrite example for send from IR message from TIMER.

--- firmware_nousb/main.c
+++ firmware_nousb/main.c
@@ -9,6 +9,7 @@
 #include <avr/io.h>
 #include <avr/wdt.h>
 #include <util/delay.h>
+#include <util/atomic.h>
 
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
@@ -18,32 +19,47 @@
 
 #include "../logick/ircoder.h"
 
-#define DDRBIT(port, num, s) DDR##port=((DDR##port & (~(1<<num))) | ((s?1:0)<<num));
-#define BIT(port, num, s) PORT##port=((PORT##port & (~(1<<num))) | ((s?1:0)<<num));
-#define SETBIT(port, num, s) port=((port & (~(1<<num))) | ((s?1:0)<<num));
+#include "support.h"
+#include "timer.h"
+
+uint8_t send_msg[8];
+uint8_t send_itr=8;
+uint8_t send_bititr=8;
+
+// INTERRUPTS!
+//ISR(TIMER2_COMPA_vect, ISR_NAKED) // XXX: NAKED
+ISR(TIMER2_COMPA_vect ) // XXX: NAKED
+{
+  static char aa=0;
+  TIFR2=0;
+  TCNT2=0;
+  aa++;
+  
+  BIT(B, 0,  aa    &1);
+  BIT(B, 4, (aa>>1)&1);
+  BIT(B, 3, (aa>>2)&1);
+ // _delay_us(50);
+  if(send_itr<8){
+    if((send_msg[send_itr]>>send_bititr)&1){
+        T0_ON();
+    }else{
+        T0_OFF();
+    }
+
+    if(send_bititr--==0){
+      send_itr++;
+      send_bititr=7;
+    }
+  }
+
+}
+
+
+//////////////////////////////
 
 int  write=0;
 unsigned char IRbytes[200];
-
 uint32_t time=0;
-#define IRBYTE1    !(PINB&2)
-#define DELAYBIT	 _delay_us(50)
-#define MAX_DELAY  0xff
-
-// GOOD
-//*
-#define T0_ON()  TCCR0B=0x0a;
-#define T0_OFF() {\
-  TCCR0B=0x00;    \
-  BIT(D, 6, 0);   \
-}
-//*/
-// BAD
-//#define T0_ON()   DDRBIT(D, 6, 0)
-//#define T0_OFF()  DDRBIT(D, 6, 1)
-// BAD
-//#define T0_ON()  power_timer0_enable()
-//#define T0_OFF() power_timer0_disable()
 
 /*
  * Parsing IR channel
@@ -73,7 +89,7 @@ uint16_t count_bit(uint8_t type, uint8_t trigger)
 bad_work:
 	//sei();
 	return count;
-} 
+}
 
 /*
  * Parsing IR channel
@@ -147,83 +163,46 @@ int main(void)
 {
     wdt_disable();
     cli();
-    
-    DDRB=0xFD;
-		PORTB=0xFF;
-    DDRC=0xFF;
-    PORTC=0;
-    DDRBIT(D, 6, 1);//=0xFD;
-    DDRBIT(D, 5, 1);//=0xFD;
 
-    //// 8 bit timer programming....
-    // Diver 8 (20000 kHZ / 8 => 2500 kHz)
-    //  needed 36 kHz.
-    // 2500 / 36.0 => 69.444 (0x45)
-    //
-    // TCCR0A
-    //  7 - COMOA1
-    //  6 - COMOA0
-    //  5 - COMOB1
-    //  4 - COMOB0
-    //  3,2
-    //  1 - WGMO1
-    //  0 - WGMO0
-    // 0b000100
-    
-    // for TCCR0A --
-    // 0b01000011 -- TIMER 0 (OC0A in toggle mode), TIMER 1 is off 
-    //         ||- WGM00 == 1 -- it needed ;-)
-    //         |- WGM01 == 0 TOP <-> BOTTOM
-    //                     1 BOTTOM <-> TOP
-    // for
-    //         | WGM02 == 1 -- its toggle OC0A it comapre match
-    //
-    //
-    // // DDR must be set (its set)
-    //*
-    TCCR0A=0x43;
-    //TCCR0B
-    // 0b??--1101
-    //   ||   | 010 -- clk/8
-    //   ||   \ 101 -- clk/1024
-    //   ||FOC0B
-    //   |FOC0A
-    TCCR0B=0x0a;//0b00001010
-    TIMSK0=0x00;//no needed interrupt
-    OCR0A=20;//0x29;
-    power_timer0_enable();
-    //*/
-   
+    DDRB=0xff;
+    // 36 kHz -- FIXME need normal name for this function.
+    timer_load();//it used PORTD -- BIT 6
+
+    // 200-600 us -- FIXME need nomral name for this function.
+    frame_timer_load();
+
     sei();
-    BIT(B, 2, 1);
-   uint8_t msg[8];
+
+    char b=0;
+   /// CODER (user, gid, gun, CRC)
+   //         00111 011  0000 1010
    uint32_t st;
+   st=0x3b0a;
+   uint8_t msg[8];
    uint8_t  *code   = (uint8_t*)  &st;
-   st=0xbeef;
    code2ir_shot(code, msg, 16);
-
-   uint8_t a=0;
-   uint8_t c=7;
-    for(;;){    /* main event loop */
-			time++;
-
-      if((msg[a]>>c)&1){
-        T0_ON();
-      }else{
-        T0_OFF();
+   /// CODER
+ //   return 0;
+    do{
+      cli();
+      if(send_itr>=8){
+        sei();
+        _delay_ms(1000);
+        cli();
+        send_msg[0]=msg[0];
+        send_msg[1]=msg[1];
+        send_msg[2]=msg[2];
+        send_msg[3]=msg[3];
+        send_msg[4]=msg[4];
+        send_msg[5]=msg[5];
+        send_msg[6]=msg[6];
+        send_msg[7]=msg[7];
+        send_itr=0;
       }
-      _delay_us(600);//TODO change it to 200
-      if(c--==0){
-        if(++a>=7){
-          a=0;
-          T0_OFF();
-          power_timer0_disable();
-          _delay_ms(100);
-          power_timer0_enable();
-        }
-        c=7;
-      }//*/
-    }
+      BIT(B, 5, b++&1);
+      sei();
+      //asm(      " sbr 
+    }while(1);
     return 0;
 }
 
