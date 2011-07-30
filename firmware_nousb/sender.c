63d64cc086a5ed281ee68375b308e3723ded1add
Leonid Myravjev <asm@asm.pp.ru>
2011-07-31 01:46:12 +0400

    Fixed sender errors (power supply and off state).

--- firmware_nousb/sender.c
+++ firmware_nousb/sender.c
@@ -1,18 +1,20 @@
 #include <stdint.h>
 //#include <stddef.h>
 #include <avr/io.h>
+#include <avr/power.h>
+
 #include "support.h"
 
 #include "sender.h"
 
 static uint8_t send_msg[8];
 static uint8_t send_itr=8;
-static uint8_t send_bititr=8;
+static uint8_t send_bititr=7;
 static uint8_t send_length=0;
 
 int send_iteration()
 {
-    if(send_itr<send_length){
+    if(send_is_freely()!=0){
       if((send_msg[send_itr]>>send_bititr)&1){
           T0_ON();
       }else{
@@ -24,10 +26,14 @@ int send_iteration()
         send_bititr=7;
       }
     }
+    if(send_is_freely()==0){
+      T0_OFF();
+      power_timer0_disable();
+    }
     return 0;
 }
 
-int send_set_message(uint8_t *msg, int count)
+int send_set_message(uint8_t *msg, uint8_t count)
 {
   // It needed ?
   //if(send_is_freely!=0)
@@ -38,6 +44,7 @@ int send_set_message(uint8_t *msg, int count)
     send_msg[count]=msg[count];// FIXME: memcpy
   }
   send_itr=0;
+  power_timer0_enable();
 
   return 0;
 }
--- firmware_nousb/sender.h
+++ firmware_nousb/sender.h
@@ -1,4 +1,4 @@
 
 int send_iteration();
-int send_set_message(uint8_t *msg, int count);
+int send_set_message(uint8_t *msg, uint8_t count);
 int send_is_freely();
