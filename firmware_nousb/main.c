e975d69bf158c4b0bad5566b86a60e9abcc168db
Leonid Myravjev <asm@asm.pp.ru>
2011-08-01 00:58:37 +0400

    Copyright added to firmware_nousb.

--- firmware_nousb/main.c
+++ firmware_nousb/main.c
@@ -1,7 +1,25 @@
-/* Name: main.c
- * Project: IrReceiver
- * Author: Leonid Myravjev
+/*
+ * main.c  - Main of firmware
  * Creation Date: 2011-05-08
+ *
+ * Copyright (C) 2011 Leonid Myravjev (asm@asm.pp.ru)
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 
 #define F_CPU 12000000UL 
